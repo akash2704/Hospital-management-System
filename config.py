b2eba5bece87e03499699da395aad830bd30f459
akash2704 <71868921+akash2704@users.noreply.github.com>
2023-05-05 13:00:08 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+password = '1234'                               
+database_name = 'database_1A'                             
+edit_mode_password = 'edit'                         
+dr_mls_access_code = 'edit'      
